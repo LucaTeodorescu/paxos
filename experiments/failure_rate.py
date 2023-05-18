295013f223c6c2a102cef09a1f0607d0b00eff83
LucaTeodorescu <lvteodorescu@gmail.com>
2023-05-18 13:41:01 +0200

    updated from avg_delay unreliable msg to max_delay

--- experiments/failure_rate.py
+++ experiments/failure_rate.py
@@ -3,6 +3,11 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
+NB_SIMULATION = 10
+FAILURE_RATE_RANGE = [0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10]
+NB_OF_ACCEPTOR = 5
+NB_PROPOSERS = 1
+
 
 class Simulation:
     # Class simulation to run a simulation
@@ -12,9 +17,9 @@ class Simulation:
                  n_acceptors: int,
                  messenger_failure_rate = 0, 
                  proposer_fail_rate=0, 
-                 messenger_avg_delay=1
+                 messenger_max_delay=0.5
                  ):
-        self.messenger = UnreliableMessenger(failure_rate=messenger_failure_rate, avg_delay=messenger_avg_delay)
+        self.messenger = UnreliableMessenger(failure_rate=messenger_failure_rate, max_delay=messenger_max_delay)
         self.assembly = Assembly(n_proposers=n_proposers, n_acceptors=n_acceptors, messenger=self.messenger, proposer_fail_rate=proposer_fail_rate)
 
     def start(self):
@@ -27,10 +32,10 @@ class Simulation:
 
 if __name__ == '__main__':
     list_time = []
-    for j in range(5):
+    for failure_rate in FAILURE_RATE_RANGE:
         results_list = []
-        for i in range(1):
-            simul = Simulation(n_proposers=1, n_acceptors=4, messenger_failure_rate = 0.01*j, proposer_fail_rate=0, messenger_avg_delay=0)
+        for i in range(NB_SIMULATION):
+            simul = Simulation(n_proposers=NB_PROPOSERS, n_acceptors=NB_OF_ACCEPTOR, messenger_failure_rate = failure_rate, proposer_fail_rate=0, messenger_max_delay=0)
             results_list.append(simul.start())
         list_time.append(np.mean(results_list))
     plt.plot(list_time)
